6f74153fb815382a845daf766c64547ca2593ca6
radityagumay <gumay.raditya@yahoo.com>
2017-04-17 18:41:43 +0700

    [ContactApp] [Task] remove unused

--- app/src/test/java/net/radityalabs/contactapp/ContactListPresenterTest.java
+++ app/src/test/java/net/radityalabs/contactapp/ContactListPresenterTest.java
@@ -20,6 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 
