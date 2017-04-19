c28655e41c900789733d549c19ed89f332e7b291
radityagumay <gumay.raditya@yahoo.com>
2017-04-19 22:33:25 +0700

    [ContactApp] [Task] Add Send and Call UI Test

--- app/src/androidTest/java/net/radityalabs/contactapp/ContactDetailFragmentTest.java
+++ app/src/androidTest/java/net/radityalabs/contactapp/ContactDetailFragmentTest.java
@@ -1,5 +1,11 @@
 package net.radityalabs.contactapp;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.os.Build;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
@@ -10,6 +16,7 @@ import net.radityalabs.contactapp.data.network.response.ContactListResponse;
 import net.radityalabs.contactapp.presentation.ui.activity.ContactDetailActivity;
 import net.radityalabs.contactapp.presentation.ui.activity.ContactListActivity;
 import net.radityalabs.contactapp.presentation.ui.fragment.ContactDetailFragment;
+import net.radityalabs.contactapp.presentation.util.PhoneUtil;
 import net.radityalabs.contactapp.test.DrawableMatcher;
 import net.radityalabs.contactapp.test.FragmentTestRule;
 
@@ -22,6 +29,8 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -36,6 +45,9 @@ import static net.radityalabs.contactapp.test.ViewAction.setTextInTextView;
 public class ContactDetailFragmentTest {
 
     private static final String STRING_TO_BE_TYPED = "raditya gumay";
+    private static final String PHONE_NUMBER = "1234567890";
+
+    private static String PACKAGE_ANDROID_DIALER = "com.android.phone";
 
     @Rule
     public FragmentTestRule<ContactDetailFragment> mContactDetailFragmentRule =
@@ -48,6 +60,12 @@ public class ContactDetailFragmentTest {
     @Rule
     public IntentsTestRule<ContactDetailActivity> mContactDetailRule =
             new IntentsTestRule<>(ContactDetailActivity.class, true, false);
+
+    static {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            PACKAGE_ANDROID_DIALER = "com.android.server.telecom";
+        }
+    }
 
     @Before
     public void setup() {
@@ -76,8 +94,52 @@ public class ContactDetailFragmentTest {
     }
 
     @Test
-    public void do_calling_test() {
+    public void send_sms_test() {
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, PhoneUtil.sms(PHONE_NUMBER));
+        intending(toPackage(PACKAGE_ANDROID_DIALER)).respondWith(result);
+        onView(withId(R.id.rv_info)).perform(RecyclerViewActions
+                .actionOnItemAtPosition(0, new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return null;
+                    }
 
+                    @Override
+                    public String getDescription() {
+                        return "Click on specific button";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        View v = view.findViewById(R.id.iv_right_icon);
+                        v.performClick();
+                    }
+                }));
+    }
+
+
+    @Test
+    public void calling_test() {
+        Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, PhoneUtil.call(PHONE_NUMBER));
+        intending(toPackage(PACKAGE_ANDROID_DIALER)).respondWith(result);
+        onView(withId(R.id.rv_info)).perform(RecyclerViewActions
+                .actionOnItemAtPosition(0, new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "Click on specific button";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        View v = view.findViewById(R.id.iv_left_icon);
+                        v.performClick();
+                    }
+                }));
     }
 
     private ContactDetailActivity startActivity() {
---
+++ app/src/androidTest/java/net/radityalabs/contactapp/test/FirstViewMatcher.java
@@ -0,0 +1,31 @@
+package net.radityalabs.contactapp.test;
+
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Created by radityagumay on 4/19/17.
+ */
+
+public class FirstViewMatcher {
+    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
+        return new TypeSafeMatcher<View>() {
+            int currentIndex = 0;
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with index: ");
+                description.appendValue(index);
+                matcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                return matcher.matches(view) && currentIndex++ == index;
+            }
+        };
+    }
+}
